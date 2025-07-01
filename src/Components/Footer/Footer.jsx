9833f78081cd96188f21be72972c65f46e18d87b
omor github <omorfaruque783@gmail.com>
2025-07-01 15:15:23 +0600

    My portfolio website

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <div id="Footer" className="flex justify-around bg-[#465697] text-white p-10 md:p-12 items-center">
       <div>
-        <h1 className="text-2xl md:text-6xl font-bold ">Contact</h1>
+        <h1 className="text-2xl md:text-6xl font-bold ">Footer</h1>
         <h3 className="text-sm md:text-2xl font-normal">Feel Free To Reach Out!</h3>
       </div>
 
